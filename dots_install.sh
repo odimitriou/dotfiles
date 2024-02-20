764aeb673f7f99be97e59f5e94d215911a2df682
Orestis Dimitriou <93268150+odimitriou@users.noreply.github.com>
2024-02-20 21:35:48 +0200

    add setup for tmux

--- dots_install.sh
+++ dots_install.sh
@@ -1,17 +1,16 @@
 # Update .bashrc
-
 mv ~/.bashrc ~/.bashrc.bak
 mv ~/tmp/.bashrc ~/.bashrc
 source ~/.bashrc
 
 # Set Alacritty configuration
-
 mkdir ~/.config/alacritty
 mv ~/tmp/alacritty.toml ~/.config/alacritty/alacritty.toml
 
 # Install themes for alacritty
-
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
-
+# Set tmux.conf
+mv ~/tmp/tmux.conf ~/.config/tmux/tmux.conf
+tmux source ~/.config/tmux/tmux.conf
