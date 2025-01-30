0323340b574bae8913f7dc11639959e43baec1e1
Orestis <dimitriou.or@gmail.com>
2025-01-30 08:29:12 +0200

    Update dots_install.sh

--- dots_install.sh
+++ dots_install.sh
@@ -1,29 +1,20 @@
 # Update .bashrc
-mv ~/.bashrc ~/.bashrc.bak
-mv ~/tmp/.bashrc ~/.bashrc
+mv ./.bashrc ~/.bashrc
 source ~/.bashrc
-
-# Set Alacritty configuration
-mkdir -p ~/.config/alacritty
-mv ~/tmp/alacritty.toml ~/.config/alacritty/alacritty.toml
 
 # Install themes for alacritty
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
+# Set Alacritty configuration
+mkdir -p ~/.config/alacritty
+mv ./alacritty.toml ~/.config/alacritty/alacritty.toml
+
 # Set tmux.conf
 mkdir -p ~/.config/tmux
-mv ~/tmp/tmux.conf ~/.config/tmux/tmux.conf
+mv ./tmux.conf ~/.config/tmux/tmux.conf
 tmux source ~/.config/tmux/tmux.conf
-
-# Wallpapers
-mkdir -p ~/Pictures
-mv ~/tmp/Wallpapers ~/Pictures/
 
 # Zathura
 mkdir -p ~/.config/zathura
-mv ~/tmp/zathurarc ~/.config/zathura/ 
-
-# Neovim
-mkdir -p ~/.config/nvim
-mv -r ~/tmp/nvim/ ~/.config/nvim
+mv ./zathurarc ~/.config/zathura/ 
