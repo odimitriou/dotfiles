011cb3027b3fafceb678e0e68b06ea6e7617f0a2
odimitriou <dimitriou.or@gmail.com>
2025-03-13 15:58:59 +0200

    Updated dots-install

--- dots_install.sh
+++ dots_install.sh
@@ -8,6 +8,11 @@ source ~/.bashrc
 midir -p ~/.config/ghostty
 mv ~/dotfiles/ghostty.conf ~/.config/ghostty/config
 
+# Yazi
+mkdir -p ~/.config/yazi
+mv ~/dotfiles/yazi-themes/ ~/.config/yazi/themes
+mv ~/dotfiles/yazi.toml ~/.config/yazi
+
 # Set tmux.conf
 mkdir -p ~/.config/tmux
 mv ~/dotfiles/tmux.conf ~/.config/tmux/tmux.conf
@@ -15,7 +20,7 @@ tmux source ~/.config/tmux/tmux.conf
 
 # Zathura
 mkdir -p ~/.config/zathura
-mv ~/dotfiles/zathurarc ~/.config/zathura/ 
+mv ~/dotfiles/zathurarc ~/.config/zathura/
 
 # NeoVim
 mv ~/.config/nvim ~/.config/nvim.bak
